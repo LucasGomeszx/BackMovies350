abc79b1ef0ab4761aa19c45fb5e07149b5a3fcb6
Lucas Gomes <lgomeszx@gmail.com>
2023-03-13 14:29:12 -0300

    Fazendo nevegacao e alterando imagens de login Apple e Google

--- BackMovies/BackMovies/Login/ViewController/LoginViewController.swift
+++ BackMovies/BackMovies/Login/ViewController/LoginViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var senhaTextField: UITextField!
+    
+    @IBOutlet weak var googleImageView: UIImageView!
+    
+    @IBOutlet weak var appleImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configurandoLoginView()
     }
 
+    func configurandoLoginView() {
+        appleImageView.clipsToBounds = true
+    }
 
 }
 
