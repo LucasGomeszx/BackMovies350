7ce6409d956c31a7e0b938bbf0bd9d666b5641f4
Lucas Gomes <lgomeszx@gmail.com>
2023-03-26 12:15:05 -0300

    Criando TableView e configuracoes iniciais

--- BackMovies/BackMovies/Search/ViewController/SearchViewController.swift
+++ BackMovies/BackMovies/Search/ViewController/SearchViewController.swift
@@ -8,22 +8,44 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var searchTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpView()
+        configureNavigation()
+        configureTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpView() {
+        mainView.backgroundColor = .black
+        titleLabel.textColor = .white
+        titleLabel.text = "Busca"
+        searchTableView.backgroundColor = .lightGray
+        searchTableView.layer.cornerRadius = 15
     }
-    */
+    
+    private func configureTableView() {
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+    }
+    
+    private func configureNavigation() {
+        navigationController?.navigationBar.isHidden = true
+    }
 
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
