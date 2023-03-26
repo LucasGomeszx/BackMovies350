e99aad9bc41b723653cdb494f1eaa74dda458d80
Lucas Gomes <lgomeszx@gmail.com>
2023-03-26 18:56:16 -0300

    Criando Cell do topo da collectionView

--- BackMovies/BackMovies/MovieDetails/ViewController/MovieDetailsViewController.swift
+++ BackMovies/BackMovies/MovieDetails/ViewController/MovieDetailsViewController.swift
@@ -9,21 +9,55 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpView()
+        setUpTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpView() {
+        mainView.backgroundColor = .black
+        titleLabel.text = "Detalhes"
+        titleLabel.textColor = .white
+        detailTableView.layer.cornerRadius = 15
+        detailTableView.backgroundColor = .gray
     }
-    */
+    
+    private func setUpTableView() {
+        detailTableView.delegate = self
+        detailTableView.dataSource = self
+        detailTableView.register(MovieTopTableViewCell.nib(), forCellReuseIdentifier: MovieTopTableViewCell.identifier)
+    }
 
+}
+
+extension MovieDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row{
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: MovieTopTableViewCell.identifier, for: indexPath) as? MovieTopTableViewCell
+            return cell ?? UITableViewCell()
+        default:
+            return UITableViewCell()
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row{
+        case 0:
+            return 520
+        default:
+            return 0
+        }
+    }
+    
 }
