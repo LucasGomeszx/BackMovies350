8a4813c967fbc47cf2bd770f94c7f8c09ea0cb8f
Lucas Gomes <lgomeszx@gmail.com>
2023-03-27 13:52:49 -0300

    Criando ActorTopCell e exibindo na TableView

--- BackMovies/BackMovies/ActorDetails/ViewController/ActorDetailViewController.swift
+++ BackMovies/BackMovies/ActorDetails/ViewController/ActorDetailViewController.swift
@@ -8,22 +8,57 @@
 import UIKit
 
 class ActorDetailViewController: UIViewController {
-
+    
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var actorTitleLabel: UILabel!
+    @IBOutlet weak var actorTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpView()
+        configureTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpView() {
+        mainView.backgroundColor = .black
+        actorTitleLabel.text = "Detalhes"
+        actorTitleLabel.textColor = .white
+        actorTableView.backgroundColor = UIColor(named: "BackGray")
+        actorTableView.layer.cornerRadius = 15
     }
-    */
+    
+    private func configureTableView() {
+        actorTableView.delegate = self
+        actorTableView.dataSource = self
+        actorTableView.register(ActorTopTableViewCell.nib(), forCellReuseIdentifier: ActorTopTableViewCell.identifier)
+    }
+    
+}
 
+//MARK: - TableViewDelegate, TableViewDataSource
+
+extension ActorDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: ActorTopTableViewCell.identifier, for: indexPath) as? ActorTopTableViewCell
+            return cell ?? UITableViewCell()
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0:
+            return 750
+        default:
+            return 0
+        }
+    }
+    
 }
