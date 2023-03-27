4b812f365022d0c4ca5b399b896bb5944e0c1ad5
Lucas Gomes <lgomeszx@gmail.com>
2023-03-27 09:41:06 -0300

    Criando arquivos da RelatedCell

---
+++ BackMovies/BackMovies/MovieDetails/Cells/TableViewCell/RelatedCell/RetatedTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RetatedTableViewCell.swift
+//  BackMovies
+//
+//  Created by Lucas Gomesx on 27/03/23.
+//
+
+import UIKit
+
+class RetatedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
