75778fa31d553097d3319dbf86d321721ef90319
Lucas Gomes <lgomeszx@gmail.com>
2023-04-07 16:11:51 -0300

    Criando Poster model

---
+++ BackMovies/BackMovies/Common/Model/PosterMovies.swift
@@ -0,0 +1,56 @@
+//
+//  PosterMovies.swift
+//  BackMovies
+//
+//  Created by Lucas Gomesx on 07/04/23.
+//
+
+import Foundation
+
+// MARK: - Movies
+struct Movies: Codable {
+    let dates: Dates
+    let page: Int
+    let results: [Poster]
+    let totalPages, totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case dates, page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Dates
+struct Dates: Codable {
+    let maximum, minimum: String
+}
+
+// MARK: - Result
+struct Poster: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let genreIDS: [Int]
+    let id: Int
+    let originalLanguage, originalTitle, overview: String
+    let popularity: Double
+    let posterPath, releaseDate, title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
