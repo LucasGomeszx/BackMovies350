bf03ce712df36466fb47ede0d8256aaf14380255
Lucas Gomes <lgomeszx@gmail.com>
2023-06-03 20:34:41 -0300

    Ativando IQKeyboardManager no projeto.

--- BackMovies/BackMovies/AppDelegate.swift
+++ BackMovies/BackMovies/AppDelegate.swift
@@ -8,12 +8,14 @@
 import UIKit
 import FirebaseCore
 import GoogleSignIn
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        IQKeyboardManager.shared.enable = true
         return true
     }
     
