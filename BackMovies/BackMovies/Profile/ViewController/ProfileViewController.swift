d86a8f6bfe9570d814e54454163d47c74368e000
Lucas Gomes <lgomeszx@gmail.com>
2023-06-30 09:34:52 -0300

    Acessando a galeria e exibindo a imagem selecionada pelo usuario na tela de perfil.

--- BackMovies/BackMovies/Profile/ViewController/ProfileViewController.swift
+++ BackMovies/BackMovies/Profile/ViewController/ProfileViewController.swift
@@ -14,34 +14,44 @@ enum ProfileStrings: String {
 }
 
 class ProfileViewController: UIViewController {
-
+    
     @IBOutlet weak var profilePhoto: UIImageView!
     @IBOutlet weak var changeImageButton: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     
     var viewModel: ProfileViewModel = ProfileViewModel()
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigation()
-        imageToCircle()
+        setupView()
         viewModel.configDelegate(delegate: self)
         viewModel.getUserData()
+        imagePicker.delegate = self
     }
     
     func configureNavigation() {
         navigationController?.navigationBar.isHidden = true
     }
     
-    func imageToCircle() {
-            profilePhoto.layer.borderWidth = 1
-            profilePhoto.layer.masksToBounds = false
-            profilePhoto.layer.borderColor = UIColor.black.cgColor
-            profilePhoto.layer.cornerRadius = profilePhoto.frame.height/2
-            profilePhoto.clipsToBounds = true
+    func setupView() {
+        let action = UITapGestureRecognizer(target: self, action: #selector(selectImageFromGallery))
+        changeImageButton.addGestureRecognizer(action)
+        changeImageButton.isUserInteractionEnabled = true
+        profilePhoto.layer.borderWidth = 1
+        profilePhoto.layer.masksToBounds = false
+        profilePhoto.layer.borderColor = UIColor.black.cgColor
+        profilePhoto.layer.cornerRadius = profilePhoto.frame.height/2
+        profilePhoto.clipsToBounds = true
     }
-
+    
+    @objc func selectImageFromGallery(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     @IBAction func tappedExitButton(_ sender: Any) {
         let logoutAction = UIAlertAction(title: ProfileStrings.alertExit.rawValue, style: .destructive) { _ in
             self.viewModel.logOut()
@@ -60,4 +70,20 @@ extension ProfileViewController: ProfileViewModelProtocol {
         nameLabel.text = name
         emailLabel.text = email
     }
+}
+
+//MARK: - ProfileViewController
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            profilePhoto.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
