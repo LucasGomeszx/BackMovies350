b5c9cfe79352b22f4756a17d4fa11120c8bc9cb9
LucasGomeszx <lgomeszx@gmail.com>
2024-06-03 16:56:57 -0300

    Adicionando SwiftUI no projeto com primeira tela e organizando projeto.

---
+++ BackMovies/BackMovies/SwiftUI/Login/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  BackMovies
+//
+//  Created by Lucas Gomesx on 03/06/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Tela Login")
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ BackMovies/BackMovies/SwiftUI/Login/ViewModel/LoginViewModelSwiftUI.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewModelSwiftUI.swift
+//  BackMovies
+//
+//  Created by Lucas Gomesx on 03/06/24.
+//
+
+import Foundation
+
+class LoginViewModelSwiftUI {
+    
+}
