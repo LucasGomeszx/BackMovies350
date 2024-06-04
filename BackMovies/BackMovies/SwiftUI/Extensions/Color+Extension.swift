c209202077d5de2ee07408459abf835a9c2955a3
LucasGomeszx <lgomeszx@gmail.com>
2024-06-04 12:24:49 -0300

    Criando tela de login.

---
+++ BackMovies/BackMovies/SwiftUI/Extensions/Color+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extension.swift
+//  BackMovies
+//
+//  Created by Lucas Gomesx on 04/06/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let backGray = Color(red: 50/255, green: 50/255, blue: 50/255)
+    static let lineGray = Color(red: 114/255, green: 114/255, blue: 114/255)
+    static let textColor = Color(red: 156/255, green: 156/255, blue: 156/255)
+    static let buttonColor = Color(red: 116/255, green: 59/255, blue: 157/255)
+    static let tabBarColor = Color(red: 40/255, green: 40/255, blue: 40/255)
+    static let lottieBack = Color(red: 50/255, green: 50/255, blue: 50/255, opacity: 0.4)
+    static let infoBack = Color(red: 50/255, green: 50/255, blue: 50/255, opacity: 0.99)
+}
--- BackMovies/BackMovies/SwiftUI/Login/View/LoginView.swift
+++ BackMovies/BackMovies/SwiftUI/Login/View/LoginView.swift
@@ -8,8 +8,133 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var emailText: String = ""
+    @State private var passwordText: String = ""
+    
     var body: some View {
-        Text("Tela Login")
+        GeometryReader { geo in
+            ZStack {
+                Image(.cinemaBack)
+                    .resizable()
+                    .scaledToFill()
+                
+                VStack {
+                    Image(.logo)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width / 1.5)
+                        .padding(.top, 100)
+                    
+                    Text("E-mail")
+                        .font(.system(size: 20, weight: .bold))
+                        .foregroundStyle(.white)
+                        .frame(width: geo.size.width - 26, alignment: .leading)
+                        .padding(.horizontal)
+                    
+                    
+                    ZStack(alignment: .leading) {
+                        if emailText.isEmpty {
+                            Text("E-mail:")
+                                .foregroundStyle(.gray.opacity(0.5))
+                                .padding(.horizontal)
+                        }
+                        TextField("", text: $emailText)
+                            .padding(.horizontal)
+                        
+                    }
+                    .frame(width: geo.size.width - 26, height:50 , alignment: .leading)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .font(.system(size: 20))
+                    .background(Color.backGray)
+                    .foregroundStyle(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                    .padding(.bottom, 10)
+                    
+                    Text("Senha")
+                        .font(.system(size: 20, weight: .bold))
+                        .foregroundStyle(.white)
+                        .frame(width: geo.size.width - 26, alignment: .leading)
+                        .padding(.horizontal)
+                    
+                    
+                    ZStack(alignment: .leading) {
+                        if passwordText.isEmpty {
+                            Text("Senha:")
+                                .foregroundStyle(.gray.opacity(0.5))
+                                .padding(.horizontal)
+                        }
+                        SecureField("", text: $passwordText)
+                            .padding(.horizontal)
+                        
+                    }
+                    .frame(width: geo.size.width - 26, height:50 , alignment: .leading)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .font(.system(size: 20))
+                    .background(Color.backGray)
+                    .foregroundStyle(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Logar")
+                            .font(.system(size: 20))
+                            .foregroundStyle(.white)
+                            .frame(width: geo.size.width / 1.45, height: 50)
+                            .background(Color.buttonColor)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
+                    .padding(.top, 40)
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Registrar")
+                            .font(.system(size: 20))
+                            .foregroundStyle(.white)
+                            .frame(width: geo.size.width / 1.45, height: 50)
+                            .background(Color.buttonColor)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Esqueceu a senha?")
+                            .fontWeight(.bold)
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.5)
+                            .frame(width: geo.size.width / 2.5, height: 30)
+                            .background(Color.black.opacity(0.6))
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                    }
+                    .padding(.top, 10)
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Label("Logar com Google", image: "googleIcon")
+                            .font(.system(size: 20))
+                            .foregroundStyle(.white)
+                            .frame(width: geo.size.width / 1.45, height: 50)
+                            .background(Color.buttonColor)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
+                    .padding(.bottom, 40)
+                    
+                    Spacer()
+                    
+                }
+            }
+            .ignoresSafeArea()
+            .frame(width: geo.size.width, height: geo.size.height)
+        }
     }
 }
 
